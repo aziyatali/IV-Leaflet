2c52eb242707d10b4eefaac513a80efcde4a7ab9
Aziyat <aziyatalik4@gmail.com>
2021-06-04 19:29:52 +0900

    version depreciated

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,18 +1,26 @@
-import React from 'react';
+import React from "react";
 
 const Loading = () => {
-    return ( 
-    <div style = {{height: "100vh", display: "flex", justifyContent: "center", alignItems: "center"}}> 
-        <div className="spinner-border text-primary" role="status">
-        <span className="visually-hidden">Loading...</span>
-        </div>
-        <div class="spinner-border text-secondary" role="status">
-        <span class="visually-hidden">Loading...</span>
-        </div>
-        <div class="spinner-border text-success" role="status">
-        <span class="visually-hidden">Loading...</span>
-        </div>
+  return (
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <div className="spinner-grow text-success" role="status">
+        <span className="sr-only">Loading...</span>
+      </div>
+      <div className="spinner-grow text-danger" role="status">
+        <span className="sr-only">Loading...</span>
+      </div>
+      <div className="spinner-grow text-info" role="status">
+        <span className="sr-only">Loading...</span>
+      </div>
+    </div>
+  );
+};
 
-    </div> );
-}
 export default Loading;
