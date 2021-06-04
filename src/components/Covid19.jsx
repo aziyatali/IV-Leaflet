71c1568c0ed0c105b78585edb28387a7bbfd40fc
Aziyat <aziyatalik4@gmail.com>
2021-06-04 17:41:26 +0900

    added the case when there is data

--- src/components/Covid19.jsx
+++ src/components/Covid19.jsx
@@ -1,8 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import Loading from './Loading';
+import CovidMap from './Covidmap';
+import Legend from './Legend';
 
 const Covid19 = () => {
-    return <Loading/>;
-}  
+    const [countries, setCountries] = useState(["korea"]);
+    return (<div>
+        {countries.length === 0 ? (
+        <Loading/>
+        ) : (
+        <div> 
+            <CovidMap/> 
+            <Legend/> 
+        </div>
+        )}
+    </div>
+    );
+}; 
  
 export default Covid19;
---
+++ src/components/Covidmap.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+const CovidMap = () => {
+    return(
+    <div>
+        Map
+    </div>
+    );
+}
+
+export default CovidMap;
