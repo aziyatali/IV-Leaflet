b0a0e7e7c3996b410ce191d7c4b3b7e21e724d0a
Aziyat <aziyatalik4@gmail.com>
2021-06-05 14:34:57 +0900

    retrieved data but just first index

--- src/tasks/LoadCountriesTask.js
+++ src/tasks/LoadCountriesTask.js
@@ -1,8 +1,7 @@
 import papa from "papaparse";
 import legendItems from "../entities/LegendItems";
 import { features } from "../data/countries.json";
-//    this.setState(features);
-
+console.log(features);
 class LoadCountryTask {
   covidUrl = "https://raw.githubusercontent.com/aziyatali/IV-Leaflet/main/src/data/owid-covid-data.csv";
    
@@ -23,14 +22,16 @@ class LoadCountryTask {
       const country = features[i];
       //console.log(country);
       const covidCountry = covidCountries.find(
-        (covidCountry) => country.properties.ISO_A3 === covidCountry.ISO3
+        (covidCountry) => country.properties.ISO_A3 === covidCountry.iso_code
       );
+      //console.log("covid_country: ", covidCountry);
 
       country.properties.confirmed = 0;
       country.properties.confirmedText = 0;
 
       if (covidCountry != null) {
-        let confirmed = Number(covidCountry.Confirmed);
+        let confirmed = Number(covidCountry.total_cases);
+        console.log("CASE: ", confirmed);
         country.properties.confirmed = confirmed;
         country.properties.confirmedText = this.#formatNumberWithCommas(
           confirmed
